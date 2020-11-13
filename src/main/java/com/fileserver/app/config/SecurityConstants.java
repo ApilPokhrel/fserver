3a4fad5bd91251637d28878f69692166ac41d024
ApilPokhrel <apilpokharel5@gmail.com>
2020-11-13 16:49:45 +0545

    video concat demuxer

--- src/main/java/com/fileserver/app/config/SecurityConstants.java
+++ src/main/java/com/fileserver/app/config/SecurityConstants.java
@@ -7,7 +7,7 @@ public  class SecurityConstants {
     }
 
     public static final String SECRET = "SECRET_KEY_FOR_MY_JWT_MANN_HAA_MANN";
-    public static final long EXPIRATION_TIME = 900000; // 15 mins
+    public static final long EXPIRATION_TIME = 9000000000L; // 15 mins
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/v1/user/";
--- src/main/java/com/fileserver/app/service/FFMPEGService.java
+++ src/main/java/com/fileserver/app/service/FFMPEGService.java
@@ -1,6 +1,9 @@
 package com.fileserver.app.service;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -35,36 +38,37 @@ public class FFMPEGService {
         ffprobe = new FFprobe("/usr/bin/ffprobe");
     }
 
-    // 4 seconds after 3 seconds from front, 3 seconds from middle, 3 seconds from 40 seconds before last
+    // 4 seconds after 3 seconds from front, 3 seconds from middle, 3 seconds from
+    // 40 seconds before last
 
-    //5sec: format, 20: format, 30:format
+    // 5sec: format, 20: format, 30:format
     public String createPreview(@NonNull String filename) {
         FFmpegProbeResult probeResult;
         try {
             probeResult = ffprobe.probe(uploadDir + "/" + filename);
         } catch (IOException e) {
-            throw new FFMPEGException(filename+ " cannot format");
+            throw new FFMPEGException(filename + " cannot format");
         }
         FFmpegFormat format = probeResult.getFormat();
         double duration = format.duration;
         List<String> clips = new ArrayList<>();
         double[] dd = generateClipTimes(duration);
-        for(double d : dd){
-            String name = RandomStringUtils.randomAlphanumeric(10);  //name of clip
+        for (double d : dd) {
+            String name = RandomStringUtils.randomAlphanumeric(10); // name of clip
             generateClip(filename, d, name);
-            clips.add(uploadDir+"/"+name);
+            clips.add(uploadDir + "/" + name);
         }
 
-        String preview = filename.replace(".mp4", "")+"_preview.mp4";
-        combineClips(clips, preview);
+        String preview = filename.replace(".mp4", "") + "_preview.mp4";
+        // combineClips(clips, preview);
+        combineClipsDemuxer(clips, preview);
         return preview;
     }
 
-
-    //10 min clip
-    private double[] generateClipTimes(double duration){
+    // 10 min clip
+    private double[] generateClipTimes(double duration) {
         double[] dd = new double[10];
-        if(duration < 10d) {
+        if (duration < 10d) {
             return new double[0];
         }
         dd[0] = 4d;
@@ -84,53 +88,76 @@ public class FFMPEGService {
         dd[6] = six;
         dd[7] = seven;
         dd[8] = eight;
-        if((duration - 40d) > 4) dd[9] = duration - 40d;
+        if ((duration - 40d) > 4)
+            dd[9] = duration - 40d;
         return dd;
     }
 
-    private void generateClip(String filename, double d, String name){
+    private void generateClip(String filename, double d, String name) {
         FFmpegExecutor executor = new FFmpegExecutor(this.ffmpeg, this.ffprobe);
 
-        FFmpegBuilder builder = new FFmpegBuilder()
-        .setStartOffset((long) d, TimeUnit.SECONDS) //offset -ss 00:03
-            .setInput(uploadDir + "/" + filename)
-            .addOutput(uploadDir + "/" + name + ".mp4")// Filename, or a FFmpegProbeResult
-            .disableAudio()
-            .addExtraArgs("-t", "1", "-c:v", "copy")
-            .done();
-            executor.createJob(builder).run();
+        FFmpegBuilder builder = new FFmpegBuilder().setStartOffset((long) d, TimeUnit.SECONDS) // offset -ss 00:03
+                .setInput(uploadDir + "/" + filename).addOutput(uploadDir + "/" + name + ".mp4")// Filename, or a
+                                                                                                // FFmpegProbeResult
+                .disableAudio().addExtraArgs("-t", "1", "-c:v", "copy").done();
+        executor.createJob(builder).run();
     }
 
-    private void combineClips(List<String> names, String name){
+    private void combineClips(List<String> names, String name) {
         FFmpegExecutor executor = new FFmpegExecutor(this.ffmpeg, this.ffprobe);
         FFmpegBuilder builder = new FFmpegBuilder();
-        for(String file : names){
-            builder.addInput(file+".mp4");
+        for (String file : names) {
+            builder.addInput(file + ".mp4");
         }
-        builder
-            .addOutput(uploadDir+"/"+name)
-            .addExtraArgs("-filter_complex", "concat=n=10:v=1:a=0", "-y")
-            .done();
-            executor.createJob(builder).run();
-            for(String file : names){
-                try {
-                    Files.deleteIfExists(Paths.get(file + ".mp4"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        builder.addOutput(uploadDir + "/" + name).addExtraArgs("-filter_complex", "concat=n=10:v=1:a=0", "-y").done();
+        executor.createJob(builder).run();
+        for (String file : names) {
+            try {
+                Files.deleteIfExists(Paths.get(file + ".mp4"));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
     }
 
-    public void generateImage(String filename, String name, double d){
+    private void combineClipsDemuxer(List<String> names, String name) {
+        FFmpegExecutor executor = new FFmpegExecutor(this.ffmpeg, this.ffprobe);
+        FFmpegBuilder builder = new FFmpegBuilder();
+        String concatTxt = uploadDir+"/"+"concat.txt";
+        try {
+            Files.deleteIfExists(Paths.get(concatTxt));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (FileWriter fw = new FileWriter(concatTxt, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw)) {
+            for (String file : names) {
+                out.println("file '" + file + ".mp4'");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        builder.addExtraArgs("-f", "concat", "-safe", "0").setInput(concatTxt).addOutput(uploadDir + "/" + name)
+                .addExtraArgs("-c", "copy").done();
+        executor.createJob(builder).run();
+        for (String file : names) {
+            try {
+                Files.deleteIfExists(Paths.get(file + ".mp4"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void generateImage(String filename, String name, double d) {
         FFmpegExecutor executor = new FFmpegExecutor(this.ffmpeg, this.ffprobe);
 
-        FFmpegBuilder builder = new FFmpegBuilder()
-            .setInput(uploadDir+"/"+filename)
-            .addOutput(uploadDir+"/"+name+".png")
-            .setStartOffset((long) d, TimeUnit.SECONDS) //offset -ss 00:03
-            .addExtraArgs("-vframes", "1")
-            .done();
-            executor.createJob(builder).run();
+        FFmpegBuilder builder = new FFmpegBuilder().setInput(uploadDir + "/" + filename)
+                .addOutput(uploadDir + "/" + name + ".png").setStartOffset((long) d, TimeUnit.SECONDS) // offset -ss
+                                                                                                       // 00:03
+                .addExtraArgs("-vframes", "1").done();
+        executor.createJob(builder).run();
     }
 
 }
